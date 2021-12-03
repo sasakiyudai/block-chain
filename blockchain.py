92c957555ec299a97b675c7b021c0eedd6dabebe
sasakiyudai <yudai14142@gmail.com>
2021-12-03 16:34:36 +0900

    that's cool

--- blockchain.py
+++ blockchain.py
@@ -191,6 +191,21 @@ def register_node():
 	}
 	return jsonify(response), 201
 
+@app.route('/nodes/resolve', methods=['GET'])
+def consensus():
+	replaced = blockchain.resolve_conflicts()
+
+	if replaced:
+		response = {
+			'message': 'chain was replaced',
+			'new_chain': blockchain.chain
+		}
+	else:
+		response = {
+			'message': 'chain was not replaced',
+			'chain': blockchain.chain
+		}
+	return jsonify(response), 200
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=5000)
